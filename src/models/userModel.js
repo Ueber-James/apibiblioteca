2ef8d8b59c8db3d89b35c171a05d1ca49dce4f1b
james <ueberjames@gmail.com>
2025-05-04 17:49:08 -0300

    usuario

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,12 +1,12 @@
 import { pool } from '../db.js';
 import bcrypt from 'bcrypt';
 
-export async function createUser(username, plainPassword) {
+export async function createUser(nome,username, plainPassword) {
   const hash = await bcrypt.hash(plainPassword, 10);
   const res = await pool.query(
     `INSERT INTO usuarios (nome, username, password) 
      VALUES ($1, $2, $3) RETURNING id, username`,
-    [username, hash]
+    [nome, username, hash]
   );
   return res.rows[0];
 }
